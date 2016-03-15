3966096fe15ac81347eb745bf77dbcfe463e5333
Sean Matson <sean.r.matson@gmail.com>
2016-03-15 01:26:38 -0700

    app.set port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html', function(err) {
         if (err) {
