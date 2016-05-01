a6cae6309043bffbce44e8abcb7a0ba760953a26
= <sean.r.matson@gmail.com>
2016-04-30 20:33:16 -0700

    now serving static files from public directory

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html', function(err) {
